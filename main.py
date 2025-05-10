756ff0796ed311db0e42536dfd589ffa6069c7ef
Hyungsoo Kim <javaos74@gmail.com>
2025-05-10 18:19:41 +0900

    국세청 사업자등록상태 조회 에이전트 생성

---
+++ main.py
@@ -0,0 +1,59 @@
+from uipath_langchain.chat.models import UiPathAzureChatOpenAI
+from uipath_langchain.chat.models import UiPathChat
+from langchain_anthropic import ChatAnthropic
+from langchain.agents import AgentExecutor, create_tool_calling_agent, tool
+from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_openai import ChatOpenAI
+from langgraph.graph import START, StateGraph, END
+from pydantic import BaseModel
+from dotenv import find_dotenv, load_dotenv
+import os, requests
+from nts_tools import nts_check_business_status_tool
+
+
+load_dotenv(find_dotenv())
+    
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant"),
+        ("placeholder", "{chat_history}"),
+        ("human", "{input}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+)
+
+llm = UiPathAzureChatOpenAI(
+#llm = ChatOpenAI(
+    model="gpt-4o-2024-08-06",
+    temperature=0,
+    max_tokens=100,
+    timeout=5,
+    max_retries=2)
+
+tools = [nts_check_business_status_tool]
+
+nts_agent = create_tool_calling_agent(llm, tools=[nts_check_business_status_tool], prompt=prompt)
+agent_executor = AgentExecutor(agent=nts_agent, tools=tools, verbose=True)
+
+
+class GraphInput(BaseModel):
+    biz_reg_no: str
+
+class GraphOutput(BaseModel):
+    status: str
+
+async def check_status(state: GraphInput) -> GraphOutput:
+    output = agent_executor.invoke( {"input": state.biz_reg_no})
+    print(output)
+    return GraphOutput(status=output["output"])
+
+builder = StateGraph(input=GraphInput, output=GraphOutput)
+
+builder.add_node("check_status", check_status);
+
+builder.add_edge(START, "check_status")
+builder.add_edge("check_status", END)
+
+graph = builder.compile()
---
+++ nts_tools.py
@@ -0,0 +1,31 @@
+from dotenv import find_dotenv, load_dotenv
+from langchain.agents import tool
+import os, requests
+
+@tool
+def nts_check_business_status_tool( reg_no: str) -> str:
+    """
+    입력한 사업자등록번호의 상태를 확인합니다.
+    """
+    body = {
+            "b_no": [reg_no.replace("-", "")]
+        }
+    url = f"https://api.odcloud.kr/api/nts-businessman/v1/status?serviceKey={os.getenv("serviceKey")}"
+    resp = requests.post(url, json=body)
+    if resp.status_code == 200:
+        data = resp.json()
+        if data.get("status_code") == "OK":
+            if "match_cnt" in data:
+                return f"요청한 사업자등록번호는 {data["data"][0]["b_no"]}는 {data["data"][0]["b_stt"]}이며 {data["data"][0]["tax_type"]}입니다."
+            else:
+                return f"요청한 사업자등록번호는 {data["data"][0]["b_no"]}는 {data["data"][0]["tax_type"]}"
+        else:
+            return f"오류가 발생했으며 오류 코드 값은 {data["status_code"]}입니다."
+    else:
+        return f"오류가 발생했으며 오류 코드 값은 {resp.status_code}입니다."
+
+    
+if __name__ == "__main__":
+    load_dotenv(find_dotenv())
+    result = nts_check_business_status_tool("142-87-00979")
+    print(result)
